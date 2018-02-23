12b2b48ee20e22b38d11e39511f0ed5fd6e50947
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-02-23 22:31:22 +0900

    Add typings

---
+++ web_typings/tone/tone.d.ts
@@ -0,0 +1,9 @@
+declare module "tone" {
+  const Transport: any;
+  const Volume: any;
+  const Synth: any;
+  const PolySynth: any;
+  const Part: any;
+  const Master: any;
+  const context: any;
+}
