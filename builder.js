e5b57e6adfcc345f8f398ead2379e6f3850beefb
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-02-27 19:54:23 +0900

    Use a webpack compiler API

--- builder.js
+++ builder.js
@@ -1,33 +1,42 @@
-const fs = require("fs");
-const path = require("path");
-const shorten = require(path.resolve(__dirname, "shortener.js"));
+const webpack = require("webpack");
+const config = require("./webpack.config.js");
+const compiler = webpack(config);
 
-const boot = fs.readFileSync("./src/boot.ts", "utf-8");
-const title = boot.match(
-  /import\s?{\s?U\s?}\s?from\s?".\/([a-zA-Z0-9_.-]*)"/
-)[1];
-console.log(`\nBuild: ${title}.html`);
+compiler.run((err, stats) => {
+  if (err != null) {
+    console.err(err);
+    return;
+  }
 
-const bundle = fs.readFileSync("./tmp/bundle.js", "utf-8");
-const u = shorten(bundle);
-const l = u.split("\n")[1];
+  const fs = require("fs");
+  const boot = fs.readFileSync("./src/boot.ts", "utf-8");
+  const title = boot.match(
+    /import\s?{\s?U\s?}\s?from\s?".\/([a-zA-Z0-9_.-]*)"/
+  )[1];
+  console.log(`\nBuild: ${title}.html`);
 
-const setupDraw = boot
-  .substr(boot.indexOf("let isEmptySoundPlayed = false;"))
-  .replace(/w\./g, "")
-  .replace('sourceText.innerText = ""', `sourceText.innerText = '${l}'`);
+  const path = require("path");
+  const shorten = require(path.resolve(__dirname, "shortener.js"));
+  const bundle = fs.readFileSync("./tmp/bundle.js", "utf-8");
+  const u = shorten(bundle);
+  const l = u.split("\n")[1];
 
-const template = fs.readFileSync("./tmp/index.html", "utf-8");
-const toneStr = 'Tone.min.js"></script>';
-const toneIndex = template.indexOf(toneStr) + toneStr.length;
+  const setupDraw = boot
+    .substr(boot.indexOf("let isEmptySoundPlayed = false;"))
+    .replace(/w\./g, "")
+    .replace('sourceText.innerText = ""', `sourceText.innerText = '${l}'`);
 
-let index = `${template.substr(0, toneIndex)}
+  const template = fs.readFileSync("./tmp/index.html", "utf-8");
+  const toneStr = 'Tone.min.js"></script>';
+  const toneIndex = template.indexOf(toneStr) + toneStr.length;
+
+  let index = `${template.substr(0, toneIndex)}
 <script>var module = {exports:{}};</script>${template.substr(toneIndex)}`;
 
-index = `${index.substr(
-  0,
-  index.indexOf('<script type="text/javascript" src="bundle.js">')
-)}
+  index = `${index.substr(
+    0,
+    index.indexOf('<script type="text/javascript" src="bundle.js">')
+  )}
 <script>
 ${u}
 </script>
@@ -36,17 +45,18 @@ ${setupDraw}
 </script>
 </body>
 </html>`.replace(
-  "<title>jsgame256</title>",
-  `<title>${title} - jsgame256</title>`
-);
+    "<title>jsgame256</title>",
+    `<title>${title} - jsgame256</title>`
+  );
 
-const headStr = "<head>";
-const headIndex = index.indexOf(headStr) + headStr.length;
+  const headStr = "<head>";
+  const headIndex = index.indexOf(headStr) + headStr.length;
 
-index = `${index.substr(0, headIndex)}
+  index = `${index.substr(0, headIndex)}
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:title" content='${title} - jsgame256 (${l.length} / 256)' />
 <meta name="twitter:description" content='${l}' />
 ${index.substr(headStr)}`;
 
-fs.writeFileSync(`./docs/${title}.html`, index);
+  fs.writeFileSync(`./docs/${title}.html`, index);
+});
