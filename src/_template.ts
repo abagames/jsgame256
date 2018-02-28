ddc45dc1077a7ea56725a56e59ce1f2ac6127cc3
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-02-28 21:21:55 +0900

    Add _template.ts

---
+++ src/_template.ts
@@ -0,0 +1,22 @@
+// aliases
+// p5.mouseIsPressed
+declare const M: boolean;
+// p5.mouseX
+declare const X: number;
+// p5.mouseY
+declare const Y: number;
+// p5.random()
+declare const R: (min: number | any[], max?: number) => any;
+// Tone.synth.triggerAttackRelease()
+declare const N: (note: number | string, duration: number | string) => void;
+// _.range()
+declare const A: (start: number, end?: number, step?: number) => number[];
+// You can use all p5.js functions
+
+// variables
+// score
+declare let S: number;
+// ticks
+declare let T: number;
+
+export function U() {}
